7d6384070b38fc17b9c41fad8859fc1e8cece12b
ppdddd <ppdddd00123@gmail.com>
2025-09-28 00:33:29 +0700

    update small

--- src/KnowledgeGraph/__init__.py
+++ src/KnowledgeGraph/__init__.py
@@ -1,5 +1,5 @@
 from .KG_Builder import KGBuilder
-from .KG_Trainer import KGTransETrainer
+from .KG_Trainer import KGTrainer
 from .ontology_mapper import OntologyMapper
 
-__all__ = ["KGTransETrainer", "KGBuilder", "OntologyMapper"]
+__all__ = ["KGTrainer", "KGBuilder", "OntologyMapper"]
